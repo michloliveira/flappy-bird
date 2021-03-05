4900989c43faae425754c7b4c9ebe7e4ed833a45
Michel Oliveira <micheltdoliveira@gmail.com>
2021-03-04 21:14:19 -0300

    add desenho bird na tela

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
- 
+console.log("klasdnasdl");
+
+const sprites = new Image(); //inicializa uma sprite
+sprites.src = "./img/sprites.png"; //carrega a imagem base
+
+const canvas = document.querySelector('canvas'); //retorna o primeiro elemento do seletor buscado
+const ctx = canvas.getContext('2d');
+
+console.log("adjfkjasfj")
+function loop(){
+    ctx.drawImage(sprites, 0, 0, 33, 24, 10, 50, 33, 24);
+    //ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+    
+    requestAnimationFrame(loop);
+}
+
+loop();
