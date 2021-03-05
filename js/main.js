b9731a4796a5a175ad686b8c1372bea07e1ff1b3
Michel Oliveira <micheltdoliveira@gmail.com>
2021-03-04 23:40:17 -0300

    add background

--- js/main.js
+++ js/main.js
@@ -33,8 +33,25 @@ const ground = {
         ctx.drawImage(sprites, ground.sx, ground.sy, ground.sWidth, ground.sHeight, ground.dx + ground.dHeight, ground.dy, ground.dWidth, ground.dHeight);
     }
 }
+const background = {
+    sx: 390, //origem X - source x
+    sy: 0 ,//origem Y - source y
+    sWidth: 275, //origem largura - source width
+    sHeight: 204, //origem altura - source height
+    dx: 0, //destino X - destination X
+    dy: canvas.height -204, //destino Y - destination Y
+    dWidth: 275, //destino largura - destination width
+    dHeight: 204, //destino Altura = Destination height
+    draw(){ // function draw
+        ctx.fillStyle = "#70C5CE"
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(sprites, background.sx, background.sy, background.sWidth, background.sHeight, background.dx, background.dy, background.dWidth, background.dHeight);
+        ctx.drawImage(sprites, background.sx, background.sy, background.sWidth, background.sHeight, background.dx + ground.dHeight, background.dy, background.dWidth, background.dHeight);
+    }
+}
 
 function loop(){
+    background.draw();
     bird.draw();
     ground.draw();
     requestAnimationFrame(loop);
